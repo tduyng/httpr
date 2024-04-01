0b6da2734c6e2e7a6ecf908091ea38b68cf0a09e
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-01 19:23:05 +0200

    feat: response with 200

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("Accepted new connection");
+                println!("Starting server on port 4221");
+
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+                println!("Sending response: {}", response);
+                _stream.write_all(response.as_bytes()).unwrap()
             }
             Err(e) => {
                 println!("Error: {}", e);
