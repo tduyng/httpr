1a0ea002305be11ddb6e5439d98f0f28bad1c0d1
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-01 19:26:04 +0200

    feat: handle GET files route

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::{
     collections::HashMap,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream}, thread,
+    env,
+    fs::File,
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    path::PathBuf,
+    thread,
 };
 
 struct HttpRequest {
@@ -33,6 +37,23 @@ struct HttpResponse {
     status_text: String,
     body: Option<String>,
     headers: HashMap<String, String>,
+    content_type: ContentType,
+}
+
+enum ContentType {
+    TextPlain,
+    ApplicationOctetStream,
+    None,
+}
+
+impl ContentType {
+    fn as_str(&self) -> &str {
+        match self {
+            ContentType::TextPlain => "text/plain",
+            ContentType::ApplicationOctetStream => "application/octet-stream",
+            ContentType::None => "",
+        }
+    }
 }
 
 impl HttpResponse {
@@ -46,8 +67,12 @@ impl HttpResponse {
             response.push_str(&format!("{}: {}\r\n", key, value))
         }
 
+        let content_type_str = self.content_type.as_str();
+        if !content_type_str.is_empty() {
+            response.push_str(&format!("Content-Type: {}\r\n", content_type_str));
+        }
+
         if let Some(body) = &self.body {
-            response.push_str("Content-Type: text/plain\r\n");
             response.push_str(&format!("Content-Length: {}\r\n\r\n", body.len()));
             response.push_str(body);
         } else {
@@ -58,14 +83,26 @@ impl HttpResponse {
     }
 }
 
+fn handle_root_request() -> HttpResponse {
+    HttpResponse {
+        protocol: "HTTP/1.1".to_string(),
+        status_code: 200,
+        status_text: "OK".to_string(),
+        body: None,
+        headers: HashMap::new(),
+        content_type: ContentType::None,
+    }
+}
+
 fn handle_echo_request(path: &str) -> HttpResponse {
-    let text = &path[6..];
+    let text = path.trim_start_matches("/echo/");
     HttpResponse {
         protocol: "HTTP/1.1".to_string(),
         status_code: 200,
         status_text: "OK".to_string(),
         body: Some(text.to_string()),
         headers: HashMap::new(),
+        content_type: ContentType::TextPlain,
     }
 }
 
@@ -77,6 +114,7 @@ fn handle_user_agent_request(headers: &HashMap<String, String>) -> HttpResponse 
         status_text: "OK".to_string(),
         body: Some(user_agent),
         headers: HashMap::new(),
+        content_type: ContentType::TextPlain,
     }
 }
 
@@ -87,10 +125,33 @@ fn handle_not_found_request() -> HttpResponse {
         status_text: "Not Found".to_string(),
         body: None,
         headers: HashMap::new(),
+        content_type: ContentType::None,
     }
 }
 
-fn handle_request(mut stream: TcpStream) {
+fn handle_file_request(directory: &str, path: &str) -> HttpResponse {
+    let filename = path.trim_start_matches("/files/");
+    let mut file_path = PathBuf::from(directory);
+    file_path.push(filename);
+
+    if let Ok(mut file) = File::open(file_path) {
+        let mut contents = String::new();
+        if file.read_to_string(&mut contents).is_ok() {
+            return HttpResponse {
+                protocol: "HTTP/1.1".to_string(),
+                status_code: 200,
+                status_text: "OK".to_string(),
+                body: Some(contents),
+                headers: HashMap::new(),
+                content_type: ContentType::ApplicationOctetStream,
+            };
+        }
+    }
+
+    handle_not_found_request()
+}
+
+fn handle_request(mut stream: TcpStream, directory: &str) {
     let request = BufReader::new(&stream)
         .lines()
         .map(|result| result.expect("Failed to read request line"))
@@ -100,14 +161,9 @@ fn handle_request(mut stream: TcpStream) {
     if let Some(http_request) = HttpRequest::parse_request(request) {
         let response = match http_request.path.as_str() {
             path if path.starts_with("/echo/") => handle_echo_request(path),
+            path if path.starts_with("/files/") => handle_file_request(directory, path),
             "/user-agent" => handle_user_agent_request(&http_request.headers),
-            "/" => HttpResponse {
-                protocol: "HTTP/1.1".to_string(),
-                status_code: 200,
-                status_text: "OK".to_string(),
-                body: None,
-                headers: HashMap::new(),
-            },
+            "/" => handle_root_request(),
             _ => handle_not_found_request(),
         };
 
@@ -118,16 +174,26 @@ fn handle_request(mut stream: TcpStream) {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let port = "4221";
     if let Ok(listener) = TcpListener::bind(format!("127.0.0.1:{}", port)) {
         println!("Connection to port: {}", port);
+
+        let directory = if let Some(dir) = args.get(2) {
+            dir.clone()
+        } else {
+            eprintln!("No directory argument provided. Using default directory.");
+            String::from("default")
+        };
 
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
                     println!("Retrieved a connection");
+                    let directory = directory.clone();
                     thread::spawn(move || {
-                        handle_request(stream);
+                        handle_request(stream, &directory);
                     });
                 }
                 Err(e) => {
