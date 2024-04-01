6f3ae3dde165e621c1499fe19be6fa14a5795f3f
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-01 19:23:48 +0200

    feat: handle response body

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use core::fmt;
 use std::{
-    io::{self, BufRead, Write},
+    collections::HashMap,
+    error::Error,
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -8,52 +9,81 @@ struct HttpResponse {
     protocol: String,
     status_code: u32,
     status_text: String,
+    body: Option<String>,
+    headers: HashMap<String, String>,
 }
 
-impl fmt::Display for HttpResponse {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {}\r\n\r\n",
+impl HttpResponse {
+    fn into_response_string(self) -> Result<String, Box<dyn Error>> {
+        let mut response = format!(
+            "{} {} {}\r\n",
             self.protocol, self.status_code, self.status_text
-        )
+        );
+
+        for (key, value) in self.headers {
+            response.push_str(&format!("{}: {}\r\n", key, value))
+        }
+
+        if let Some(body) = self.body {
+            response.push_str("Content-Type: text/plain\r\n");
+            response.push_str(&format!("Content-Length: {}\r\n\r\n", body.len()));
+            response.push_str(&body);
+        } else {
+            response.push_str("\r\n");
+        }
+
+        Ok(response)
+    }
+}
+
+fn not_found_error(protocol: &str) -> HttpResponse {
+    HttpResponse {
+        protocol: protocol.to_string(),
+        status_code: 404,
+        status_text: "Not Found".to_string(),
+        body: None,
+        headers: HashMap::new(),
     }
 }
 
 fn handle_request(mut stream: TcpStream) {
-    let reader = io::BufReader::new(&stream);
-    let request: Vec<_> = reader
+    let request = BufReader::new(&stream)
         .lines()
-        .map(|result| result.unwrap())
+        .map(|result| result.expect("Failed to read request line"))
         .take_while(|line| !line.is_empty())
-        .collect();
-    let start_line = request.first().unwrap().to_string();
-    let mut line_parts = start_line.split(' ');
-    let method = line_parts.next().unwrap();
+        .collect::<Vec<_>>();
+
+    let fist_line = request.first().unwrap().to_string();
+    let mut line_parts = fist_line.splitn(3, ' '); // Split with limit 3
+    let _method = line_parts.next().unwrap();
     let path = line_parts.next().unwrap();
     let protocol = line_parts.next().unwrap();
 
     let response = match path {
+        path if path.starts_with("/echo/") => {
+            let text = &path[6..];
+            HttpResponse {
+                protocol: protocol.to_string(),
+                status_code: 200,
+                status_text: "OK".to_string(),
+                body: Some(text.to_string()),
+                headers: HashMap::new(),
+            }
+        }
         "/" => HttpResponse {
             protocol: protocol.to_string(),
             status_code: 200,
             status_text: "OK".to_string(),
+            body: None,
+            headers: HashMap::new(),
         },
-        _ => HttpResponse {
-            protocol: protocol.to_string(),
-            status_code: 404,
-            status_text: "Not Found".to_string(),
-        },
+        _ => not_found_error(protocol),
     };
 
-    let response_string = format!("{}", response);
-    println!(
-        "{} {} {} {}\r\n\r\n",
-        method, protocol, response.status_code, response.status_text
-    );
+    let response_string = response.into_response_string().unwrap();
 
     stream
-        .write_all(response_string.as_bytes())
+        .write_all(&response_string.into_bytes())
         .expect("Failed to write HttpResponse");
 }
 
