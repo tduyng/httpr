8633ca5e4a93aca855af6380ba28ae192ee74786
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-01 19:23:26 +0200

    feat: handle response not found

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,74 @@
-use std::{io::Write, net::TcpListener};
+use core::fmt;
+use std::{
+    io::{self, BufRead, Write},
+    net::{TcpListener, TcpStream},
+};
+
+struct HttpResponse {
+    protocol: String,
+    status_code: u32,
+    status_text: String,
+}
+
+impl fmt::Display for HttpResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {} {}\r\n\r\n",
+            self.protocol, self.status_code, self.status_text
+        )
+    }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let reader = io::BufReader::new(&stream);
+    let request: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let start_line = request.first().unwrap().to_string();
+    let mut line_parts = start_line.split(' ');
+    let method = line_parts.next().unwrap();
+    let path = line_parts.next().unwrap();
+    let protocol = line_parts.next().unwrap();
+
+    let response = match path {
+        "/" => HttpResponse {
+            protocol: protocol.to_string(),
+            status_code: 200,
+            status_text: "OK".to_string(),
+        },
+        _ => HttpResponse {
+            protocol: protocol.to_string(),
+            status_code: 404,
+            status_text: "Not Found".to_string(),
+        },
+    };
+
+    let response_string = format!("{}", response);
+    println!(
+        "{} {} {} {}\r\n\r\n",
+        method, protocol, response.status_code, response.status_text
+    );
+
+    stream
+        .write_all(response_string.as_bytes())
+        .expect("Failed to write HttpResponse");
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let port = "4221";
+    let listener =
+        TcpListener::bind(format!("127.0.0.1:{port}")).expect("Failed to make connection");
+    println!("Connection to port: {}", port);
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                println!("Accepted new connection");
-                println!("Starting server on port 4221");
+            Ok(stream) => {
+                println!("Retrieved a connection");
 
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-                println!("Sending response: {}", response);
-                _stream.write_all(response.as_bytes()).unwrap()
+                handle_request(stream)
             }
             Err(e) => {
                 println!("Error: {}", e);
