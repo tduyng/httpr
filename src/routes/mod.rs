f077968445c3feee1c37eac32d6ce4d306872e86
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-14 22:51:13 +0200

    feat: add user-agent route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,14 +5,17 @@ use crate::Result;
 mod echo;
 mod not_found;
 mod root;
+mod user_agent;
 
 pub use echo::*;
 pub use not_found::*;
 pub use root::*;
+pub use user_agent::*;
 
 pub async fn handle_routes(request: &Request) -> Result<Response> {
     match request.path.to_lowercase().as_str() {
         p if p.starts_with("/echo/") => handle_echo(request).await,
+        "/user-agent" => handle_user_agent(request).await,
         "/" => handle_root().await,
         _ => handle_not_found().await,
     }
---
+++ src/routes/user_agent.rs
@@ -0,0 +1,17 @@
+use crate::{request::Request, response::Response, Result};
+
+pub async fn handle_user_agent(request: &Request) -> Result<Response> {
+    let user_agent = match request
+        .headers
+        .iter()
+        .find(|(name, _)| name.eq_ignore_ascii_case("User-Agent"))
+    {
+        Some((_, value)) => String::from_utf8_lossy(value).into_owned(),
+        None => return Err("User-Agent header not found".into()),
+    };
+
+    Ok(Response::new()
+        .body_str(&user_agent)
+        .status_code(200, "OK")
+        .header("Content-Type", "text/plain"))
+}
