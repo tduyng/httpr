40ac3fd82e1699d37bdc88677b7ce5465f87ea23
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-30 22:18:03 +0200

    fix: remo tokio:main and use limited tokio features

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
 use rhhtp::HttpServer;
 use std::net::SocketAddr;
+use tokio::runtime;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let server = HttpServer::new();
+    let rt = runtime::Runtime::new()?;
     let address: SocketAddr = "[::1]:2024".parse()?;
-    server.listen(address).await
+    rt.block_on(server.listen(address))
 }
