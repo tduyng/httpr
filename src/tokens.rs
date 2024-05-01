42f79f2e32cb06f54c45a89e94f8f10c742cc073
tduyng <tienduy.nguyen.dev@gmail.com>
2024-05-01 11:04:06 +0200

    test: add tokens tests

--- src/tokens.rs
+++ src/tokens.rs
@@ -29,3 +29,126 @@ pub fn is_header_value_token(ch: u8) -> bool {
         _ => false,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(b'A')]
+    #[case(b'Z')]
+    #[case(b'a')]
+    #[case(b'z')]
+    #[case(b'0')]
+    #[case(b'9')]
+    #[case(b'!')]
+    #[case(b'#')]
+    #[case(b'$')]
+    #[case(b'%')]
+    #[case(b'&')]
+    #[case(b'\'')]
+    #[case(b'*')]
+    #[case(b'+')]
+    #[case(b'-')]
+    #[case(b'.')]
+    #[case(b'^')]
+    #[case(b'_')]
+    #[case(b'`')]
+    #[case(b'|')]
+    #[case(b'~')]
+    #[case(b'/')]
+    #[case(b'@')]
+    fn test_is_token(#[case] input: u8) {
+        assert!(is_token(input));
+    }
+
+    #[rstest]
+    #[case(b'A')]
+    #[case(b'Z')]
+    #[case(b'a')]
+    #[case(b'z')]
+    #[case(b'0')]
+    #[case(b'9')]
+    #[case(b'!')]
+    #[case(b'#')]
+    #[case(b'$')]
+    #[case(b'%')]
+    #[case(b'&')]
+    #[case(b'\'')]
+    #[case(b'*')]
+    #[case(b'+')]
+    #[case(b'-')]
+    #[case(b'.')]
+    #[case(b'^')]
+    #[case(b'_')]
+    #[case(b'`')]
+    #[case(b'|')]
+    #[case(b'~')]
+    fn test_is_uri_token(#[case] input: u8) {
+        assert!(is_uri_token(input));
+    }
+
+    #[rstest]
+    #[case(b'A')]
+    #[case(b'Z')]
+    #[case(b'a')]
+    #[case(b'z')]
+    #[case(b'0')]
+    #[case(b'9')]
+    #[case(b'!')]
+    #[case(b'#')]
+    #[case(b'$')]
+    #[case(b'%')]
+    #[case(b'&')]
+    #[case(b'_')]
+    #[case(b'^')]
+    #[case(b'`')]
+    #[case(b'|')]
+    #[case(b'~')]
+    #[case(b'/')]
+    fn test_is_header_name_token(#[case] input: u8) {
+        assert!(is_header_name_token(input));
+    }
+
+    #[rstest]
+    #[case(b'\t')]
+    #[case(b' ')]
+    #[case(b'!')]
+    #[case(b'#')]
+    #[case(b'$')]
+    #[case(b'%')]
+    #[case(b'&')]
+    #[case(b'\'')]
+    #[case(b'*')]
+    #[case(b'+')]
+    #[case(b'-')]
+    #[case(b'.')]
+    #[case(b'^')]
+    #[case(b'_')]
+    #[case(b'`')]
+    #[case(b'|')]
+    #[case(b'~')]
+    #[case(b'/')]
+    #[case(b':')]
+    #[case(b';')]
+    #[case(b'@')]
+    #[case(b'[')]
+    #[case(b']')]
+    #[case(b'(')]
+    #[case(b')')]
+    #[case(b',')]
+    #[case(b'?')]
+    #[case(b'=')]
+    fn test_is_header_value_token(#[case] input: u8) {
+        assert!(is_header_value_token(input));
+    }
+
+    #[rstest]
+    #[case(0x00)]
+    #[case(0x1F)]
+    #[case(0x7F)]
+    fn test_is_not_header_value_token(#[case] input: u8) {
+        assert!(!is_header_value_token(input));
+    }
+}
